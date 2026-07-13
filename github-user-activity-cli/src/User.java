53422164078096a17bf9bfd01e7d073cc105da35
MiltonRodx <uztracker.herbs129@slmails.com>
2026-07-12 22:58:16 -0300

    Add cool README.md

--- github-user-activity-cli/src/User.java
+++ github-user-activity-cli/src/User.java
@@ -1,4 +1,9 @@
 /* Only attributes, trying to modelate the Json answer when calling the
 GitHub REST API. I need to modelate that answer */
 
-public record User(String login, int id, String name, int followers);
+public record User(
+    String login,
+    int id,
+    String name,
+    int followers
+) {}
