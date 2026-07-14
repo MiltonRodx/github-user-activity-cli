a5ae86702b75d96028476bf9553409ef79a332cc
MiltonRodx <uztracker.herbs129@slmails.com>
2026-07-14 16:17:49 -0300

    xd

--- github-user-activity-cli/src/main/java/App.java
+++ github-user-activity-cli/src/main/java/App.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 /* This is my App.java */
 public class App {
+
+    private static final String GITHUB_API_URL = "https://api.github.com/users/";
+
     public static void main(String[] args) throws Exception {
         
         GitHubClient client = new GitHubClient();
@@ -14,7 +17,7 @@ public class App {
         if (args.length == 1) { //check greater or equal
             try {
                 String username = args[0];
-                String url = "https://api.github.com/users/" + username + "/events";
+                String url = GITHUB_API_URL + username + "/events";
                 
                 String answer = client.returnJsonString(url); // get raw json
                 
